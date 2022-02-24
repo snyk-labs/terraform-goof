0c9191563392e39c9f9addaf2d7ad3c66589ac29
Rick Harp <rick@Ricks-MacBook-Pro.local>
2022-02-24 11:21:54 -0600

    adding changes to tf config files for version change requirements

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,6 +1,17 @@
 resource "aws_subnet" "main" {
   vpc_id     = var.vpc_id
-  cidr_block = var.cidr
+  cidr_block = var.cidr_main
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "secondary" {
+  vpc_id     = var.vpc_id
+  cidr_block = var.cidr_secondary
+  availability_zone = "${var.region}c"
 
   tags = {
     Name = "Main"
